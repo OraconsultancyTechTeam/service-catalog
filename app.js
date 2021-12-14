c5191b6d1ba1a96c7064111bd82cf5b0a42cc63c
Dan <Danyalahmed1998@gmail.com>
2021-12-14 14:32:02 +0000

    Homepage Created
    At the moment the index.ejs page is populated by the backend code, so none of the cards are hard coded. The cards are also clickable which will help in collecting the data later on

--- app.js
+++ app.js
@@ -9,14 +9,17 @@ const app = express()
 const port = 3000
 
 //define paths for express config
-const publicDirectoryPath = path.join(__dirname,'../public')
-const viewsPath = path.join(__dirname,'../views')
+const publicDirectoryPath = path.join(__dirname,'public')
+const viewsPath = path.join(__dirname,'../service-catalog/views')
+console.log("Directory Name: "+ viewsPath)
 
 //setup handlebars engine and views location
 app.set('view engine','ejs')
+app.set('views',viewsPath)
 
 //setup static directory to serve
-app.use(express.static(publicDirectoryPath))
+//app.use(express.static(publicDirectoryPath))
+app.use(express.static(__dirname + '/public'));
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
@@ -26,10 +29,17 @@ app.use(methodOverride('_method'));
 
 app.get('/',(req,res)=>{
 
-    res.render('index',{
-        
-        title:'User App',
-        name:'Dan Bear'
+    const catalog = [
+        {img:'/img/OnPremDatabaseLogo.png', name:'On-Prem Database', text:'The term "on premises" refers to local hardware, meaning data is stored on local servers, computers or other devices.', id:'1'},
+        {img:'/img/CloudDatabaseLogo.png', name:'Cloud Database', text:'A cloud database is a database that typically runs on a cloud computing platform and access to the database is provided as-a-service.', id:'2'},
+        {img:'/img/GoldenGate.png', name:'Replication - Goldengate', text:'Oracle GoldenGate enables you to replicate data between Oracle databases to other supported heterogeneous database, and between heterogeneous databases.', id:'3'},
+        {img:'/img/ApacheNifi.png', name:'Replication - Nifi', text:'Apache NiFi is a software project from the Apache Software Foundation designed to automate the flow of data between software systems. ', id:'4'},
+        {img:'/img/ApacheKafka.png', name:'Replication - Kafka', text:'Apache Kafka is a framework implementation of a software bus using stream-processing. It is an open-source software platform developed by the Apache Software Foundation written in Scala and Java.', id:'5'},
+      
+    ]
+    res.render('index',{   
+        title:'Service Catalog',
+        catalog
     }
     )
     
