578c0190a50a68d10e8c4a8203a96b6d9d9b6577
Dan <Danyalahmed1998@gmail.com>
2022-01-27 15:10:26 +0000

    Alerts on index page fixed and USER/ADMIN profile page

--- routers/catalog.js
+++ routers/catalog.js
@@ -18,38 +18,7 @@ connection.query(`select * from catalog_services where status=1`, (err,res) => {
     return step1 = res
 })
 
-// const step2 = [
-//     {img:'/img/oracledatabase.png', name:'Oracle Database', text:'Oracle Database is a multi-model database management system produced and marketed by Oracle Corporation', id:'6', g:'2'},
-//     {img:'/img/sql-server.png', name:'SQL Server', text:'Microsoft SQL Server is a relational database management system developed by Microsoft.', id:'7', g:'2'},
-//     {img:'/img/mysqllogo.png', name:'My SQL', text:'MySQL is an open-source relational database management system.', id:'8', g:'2'},
-//     {img:'/img/apachecassandra.png', name:'Apache Cassandra', text:'Cassandra is a free and open-source, distributed, wide-column store, NoSQL database management system designed to handle large amounts of data across many commodity servers, providing high availability with no single point of failure.', id:'9', g:'2'},
-//     {img:'/img/redislogo.png', name:'Redis', text:'Redis is an in-memory data structure store, used as a distributed, in-memory key–value database, cache and message broker, with optional durability. ', id:'10', g:'2'},
-// ]
 
-// const step3 = [
-//     {name:'PRD', id:1}, 
-//     {name:'TRG', id:2},
-//     {name:'UAT', id:3}, 
-//     {name:'DVP', id:4}
-// ]
-
-// const step4 = [
-//     {size:'2 CPU',id:1},
-//     {size:'4 CPU',id:2},
-//     {size:'6 CPU',id:3},
-//     {size:'8 CPU',id:4},
-//     {size:'10 CPU',id:5},
-//     {size:'12 CPU',id:6},
-//     {size:'24 CPU',id:7},
-//     {size:'30 CPU',id:8},
-//     {size:'Custom - Please provide comment',id:9}
-// ]
-
-// const step6 = [
-//     {name:'Enterprise Edition',id:1},
-//     {name:'Advanced Security',id:2},
-//     {name:'Advanced Compression',id:3}
-// ]
 
 //Gets Logged Out Home Page
 router.get('/', (req, res) => {
@@ -59,7 +28,6 @@ router.get('/', (req, res) => {
     })
 })
 
-let catalogMessage
 //Gets Catalog Page for logged in users
 // =====================================
 // PROFILE SECTION =========================
@@ -67,16 +35,13 @@ let catalogMessage
 // we will want this protected so you have to be logged in to visit
 // we will use route middleware to verify this (the isLoggedIn function)
 router.get('/catalog', isLoggedIn, function(req, res) {
+    const catalogMessages = req.flash('catalogMessage');
     res.render('index',{
         title: 'Service Catalog',
         msg1:'',
         step1,
         stages,
-        // step2,
-        // step3,
-        // step4,
-        // step6,
-        messages: req.flash('catalogMessage')
+        catalogMessages
     });
 });
 
@@ -135,16 +100,14 @@ router.get('/profile',isLoggedIn, (req, res) => {
     req.flash('message')
     permission = req.user.permissions
     if (permission == 1) {
-        res.render('profile', {
+        res.render('userprofile', {
             title: 'Service Catalog',
             user : req.user
         })
     } else {
-        res.render('index', {
+        res.render('adminprofile', {
             title: 'Service Catalog',
-            user : req.user,
-            step1,
-            stages
+            user : req.user
         })
     }
     
@@ -166,19 +129,26 @@ router.post('/submit', (req,res) => {
 
     if (!validator.isEmail(mang_email)) {
         req.flash('catalogMessage', 'Please enter the manager email in a correct email format')
-        messages = req.consumeFlash('catalogMessage')
-        
-        return res.redirect('/catalog', { messages })
+        return res.redirect('/catalog')
     
     } else {
-        tshirt = tshirt.match(/\d+/g)
+        
+ 
+       //if user chooses custom, set cpu value to 1 
+        if(tshirt=='Custom'){
+            tshirt = 1;
+         
+        }
+        else{
+            tshirt = tshirt.match(/\d+/g)
+
+        }
+
         const sql = "insert into requests values(null,'"+host+"','"+db+"','"+env+"','"+tshirt+"','"+dbsize+"','"+licence+"','"+comment+"','"+due_by+"','"+req_by+"','"+mang_email+"',default)";
         connection.query(sql,(err,rows,fields)=>{
             if(err) throw err
             req.flash('catalogMessage', 'Submission has been sent successfully')
-            messages = req.consumeFlash('catalogMessage')
-
-            return res.redirect('/catalog', { messages })
+            return res.redirect('/catalog')
         })
     }   
 })
